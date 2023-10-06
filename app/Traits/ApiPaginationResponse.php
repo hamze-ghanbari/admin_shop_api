08b10fa9a21a224ad68e5bb4c91e76548fb62064
hamze ghanbari <infogh1000@gmail.com>
2023-10-06 17:05:26 +0330

    create ApiPaginationResponse trait

---
+++ app/Traits/ApiPaginationResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+
+trait ApiPaginationResponse
+{
+
+    public function paginationInformation($request, $paginated, $default)
+    {
+        unset($default['meta']['links']);
+        unset($default['links']);
+        return $default;
+    }
+
+    public function with(Request $request)
+    {
+        return [
+            'status' => 200,
+            'hasError' => false,
+            'message' => null,
+        ];
+    }
+
+}
