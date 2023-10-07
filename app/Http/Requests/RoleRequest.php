7b89a324c747613642665db1b884b995c3627a06
hamze ghanbari <infogh1000@gmail.com>
2023-10-07 21:53:31 +0330

    create role request file

--- app/Http/Requests/RoleRequest.php
+++ app/Http/Requests/RoleRequest.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\BlackListRule;
+use App\Traits\ValidationResponse;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RoleRequest extends FormRequest
 {
+    use ValidationResponse;
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -22,7 +25,26 @@ class RoleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'max:30', 'regex:/^([a-z A-Z]{1,30})$/'],
+            'persian_name' => ['required', 'max:30', 'regex:/^([ ضصثقفغعهخحجچشسیبلاتنمکگپظطزرذدئو.ءِ]{1,30})$/'],
+            'status' => [new BlackListRule()]
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => 'نام نقش',
+            'persian_name' => 'نام نقش',
+            'status' => 'وضعیت نقش'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.regex' => 'نام نقش باید به صورت حروف انگلیسی باشد',
+            'persian_name.regex' => 'نام نقش باید به صورت حروف فارسی باشد',
         ];
     }
 }
