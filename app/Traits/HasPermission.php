a51b77f894915d6eed4d83057213c48f39bcfd9c
hamze ghanbari <infogh1000@gmail.com>
2023-10-08 21:54:35 +0330

    create hasPermission trait

---
+++ app/Traits/HasPermission.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits;
+
+trait HasPermission
+{
+
+    public function hasPermission($permission)
+    {
+        return (bool)$this->permissions->where('name', $permission->name)->count();
+    }
+
+    public function hasPermissionTo($permission)
+    {
+        return $this->hasPermission($permission) || $this->hasPermissionThroghRole($permission);
+    }
+
+    public function hasRole(...$roles)
+    {
+        foreach ($roles as $role) {
+            if ($this->roles->contains('name', $role))
+                return true;
+        }
+        return false;
+    }
+
+    public function hasPermissionThroghRole($permission)
+    {
+        foreach ($permission->roles as $role) {
+            if ($this->roles->contains($role))
+                return true;
+        }
+        return false;
+    }
+}
