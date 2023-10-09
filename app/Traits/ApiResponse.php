4ab2d34715141f1e1d4aef2c4daaf349703e6da1
hamze ghanbari <infogh1000@gmail.com>
2023-10-09 22:29:50 +0330

    add new methods to ApiResponse

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -2,18 +2,35 @@
 
 namespace App\Traits;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 trait ApiResponse
 {
 
-    public function apiResponse(array | object | null $result, int $status = 200, string $message = null, $hasError = false): \Illuminate\Http\JsonResponse
+    public function apiResponse(array | object | null $result, int $status = 200, string $message = null, $hasError = false): JsonResponse
     {
-        $response = [];
-        $response['status'] = $status;
-        $response['hasError'] = $hasError;
-        $response['message'] = $message;
-        $response['result'] = $result;
+        return response()->json([
+            'status' => $status,
+            'hasError' => $hasError,
+            'message' => $message,
+            'result' => $result
+        ]);
+    }
 
-        return response()->json($response);
+    public function forbiddenResponse(): JsonResponse
+    {
+        return $this->apiResponse(null, 403, 'forbidden', true);
+    }
+
+    public function serverError($message = 'خطای سرور'): JsonResponse
+    {
+        return $this->apiResponse(null, 500, $message, true);
+    }
+
+    public function sameUser($userId): bool
+    {
+        return auth()->user() === $userId;
     }
 
 }
