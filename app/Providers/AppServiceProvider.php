6c1078754c9a50ed3c0e5a19e867a8a24f01978e
hamze ghanbari <infogh1000@gmail.com>
2023-10-11 23:35:38 +0330

    remove extra codes in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
      */
@@ -21,15 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(Request $request): void
     {
         JsonResource::withoutWrapping();
-
-        Request::macro('fields', function ($guarded = ['id'], $attributes = []) use ($request) {
-            $except = ['_token', '_method'];
-            array_push($except, ...$guarded);
-            $result = $request->except($except);
-            if(!empty($attributes)){
-                $result =  array_merge($request->except($except), $attributes);
-            }
-            return $result;
-        });
     }
 }
