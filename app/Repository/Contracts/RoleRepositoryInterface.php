38919737d63717bb7e4d39d5118b01d4fc9c6a1e
hamze ghanbari <infogh1000@gmail.com>
2023-10-12 22:30:53 +0330

    update RoleRepository and UserRespository interface

--- app/Repository/Contracts/RoleRepositoryInterface.php
+++ app/Repository/Contracts/RoleRepositoryInterface.php
@@ -6,4 +6,7 @@ interface RoleRepositoryInterface extends BaseRepositoryInterface
 {
     public function checkRole($name, $persianName);
 
+    public function getRoles();
+
+    public function getRoleSearch($value);
 }
--- app/Repository/Contracts/UserRepositoryInterface.php
+++ app/Repository/Contracts/UserRepositoryInterface.php
@@ -4,5 +4,9 @@ namespace App\Repository\Contracts;
 
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
+    public function getUserByField($field, $value);
+
     public function notionalCodeExists($nationalCode);
+
+    public function getUserSearch($value);
 }
