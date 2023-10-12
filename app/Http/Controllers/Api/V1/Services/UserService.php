1984ad20cd8717728ad0e1963c1c0c3760ccf544
hamze ghanbari <infogh1000@gmail.com>
2023-10-12 22:33:27 +0330

    create searchUser method and update addRoleToUser method in UserService

--- app/Http/Controllers/Api/V1/Services/UserService.php
+++ app/Http/Controllers/Api/V1/Services/UserService.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Api\V1\Services;
 use App\Http\Requests\UserRequest;
 use App\Http\Services\CacheApiService\CacheApiService;
 use App\Models\User;
+use App\Repository\Contracts\RoleRepositoryInterface;
+use App\Repository\Contracts\UserRepositoryInterface;
 use App\Repository\Eloquent\UserRepository;
 
 class UserService
 {
     public function __construct(
-        public UserRepository  $userRepository,
-        public CacheApiService $cacheApiService
+        public UserRepositoryInterface $userRepository,
+        public RoleRepositoryInterface $roleRepository,
+        public CacheApiService         $cacheApiService
     )
     {
     }
@@ -24,11 +27,22 @@ class UserService
         return $this->userRepository->paginate();
     }
 
+    public function searchUser($value){
+       return $this->userRepository->getUserSearch($value);
+    }
+
     public function addRoleToUser(UserRequest $request, User $user)
     {
+        $syncRoles = [];
+        $rolesHasPermissions = $this->roleRepository->getRoles();
         $inputs = $request->all();
         $inputs['roles'] ??= [];
-        $user->roles()->sync($inputs['roles']);
+        foreach ($inputs['roles'] as $role) {
+            if (in_array($role, $rolesHasPermissions)) {
+                array_push($syncRoles, $role);
+            }
+        }
+        return $user->roles()->sync($syncRoles);
     }
 
     public function addPermissionToUser(UserRequest $request, User $user)
