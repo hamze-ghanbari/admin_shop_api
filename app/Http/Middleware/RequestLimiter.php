823be9d71f88972e3b29925ebe628effa404afc5
hamze ghanbari <infogh1000@gmail.com>
2023-10-13 20:17:03 +0330

    create RequestLimiter middleware

---
+++ app/Http/Middleware/RequestLimiter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Traits\ApiResponse;
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Symfony\Component\HttpFoundation\Response;
+
+class RequestLimiter
+{
+    use ApiResponse;
+
+    public function handle(Request $request, Closure $next, int $attempt): Response
+    {
+        $key = 'otp:' . url()->current() . $request->ip();
+        $executed = RateLimiter::attempt(
+            $key,
+            $attempt,
+            function () {}
+        );
+        if (!$executed) {
+            $time = ((Carbon::now())->addSeconds(RateLimiter::availableIn($key))->timestamp - Carbon::now()->timestamp);
+            $url = url()->current();
+           return $this->tooManyRequests([
+                'time' => $time,
+                'url' => $url
+            ]);
+        }
+
+        return $next($request);
+    }
+}
