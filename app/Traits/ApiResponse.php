fb71f645a8f83205b284c46082a6a0d671de4d20
hamze ghanbari <infogh1000@gmail.com>
2023-10-13 20:12:55 +0330

    add tooManyRequests to ApiResponse

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -18,9 +18,9 @@ trait ApiResponse
         ]);
     }
 
-    public function forbiddenResponse(): JsonResponse
+    public function forbiddenResponse($message = 'عدم دسترسی'): JsonResponse
     {
-        return $this->apiResponse(null, 403, 'forbidden', true);
+        return $this->apiResponse(null, 403, $message, true);
     }
 
     public function serverError($message = 'خطای سرور'): JsonResponse
@@ -28,9 +28,9 @@ trait ApiResponse
         return $this->apiResponse(null, 500, $message, true);
     }
 
-    public function sameUser($userId): bool
+    public function tooManyRequests($result, $message = 'تعداد بیش از حد درخواست'): JsonResponse
     {
-        return auth()->user() === $userId;
+        return $this->apiResponse($result, 429, $message);
     }
 
 }
