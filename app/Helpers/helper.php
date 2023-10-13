2a1f78b78724907f636e4d059814fe6537a3f0e8
hamze ghanbari <infogh1000@gmail.com>
2023-10-13 20:14:39 +0330

    remove extra code in helper file

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,22 +1,8 @@
 <?php
 
-use Morilog\Jalali\Jalalian;
-
-
 function randomNumber($count = 5): string
 {
     return substr(rand(0, microtime(true)), 0, $count);
-}
-
-function result($ajaxResponse, $redirectResponse)
-{
-    return \Illuminate\Support\Facades\Request::ajax() ? $ajaxResponse : $redirectResponse;
-}
-
-
-function jalaliDate($date, $format = '%A %d %B %Y')
-{
-    return Jalalian::forge($date)->format($format);
 }
 
 function convertNumbersToEnglish($string) {
@@ -36,39 +22,6 @@ function convertNumbersToEnglish($string) {
     return str_replace($persian, $newNumbers, $string);
 }
 
-//function convertPersianToEnglish($number): array|string
-//{
-//    $numbers = str_replace(['۰', '۱', '۲', '', '', '', '', '', '', ''], []);
-//    $number = str_replace('۰', '0', $number);
-//    $number = str_replace('۱', '1', $number);
-//    $number = str_replace('۲', '2', $number);
-//    $number = str_replace('۳', '3', $number);
-//    $number = str_replace('۴', '4', $number);
-//    $number = str_replace('۵', '5', $number);
-//    $number = str_replace('۶', '6', $number);
-//    $number = str_replace('۷', '7', $number);
-//    $number = str_replace('۸', '8', $number);
-//    $number = str_replace('۹', '9', $number);
-//
-//    return $number;
-//}
-//
-//function convertArabicToEnglish($number): array|string
-//{
-//    $number = str_replace('٠', '0', $number);
-//    $number = str_replace('١', '1', $number);
-//    $number = str_replace('٢', '2', $number);
-//    $number = str_replace('٣', '3', $number);
-//    $number = str_replace('٤', '4', $number);
-//    $number = str_replace('٥', '5', $number);
-//    $number = str_replace('٦', '6', $number);
-//    $number = str_replace('٧', '7', $number);
-//    $number = str_replace('٨', '8', $number);
-//    $number = str_replace('٩', '9', $number);
-//
-//    return $number;
-//}
-
 function convertEnglishToPersian($number): array|string
 {
     $number = str_replace('0', '۰', $number);
@@ -84,15 +37,6 @@ function convertEnglishToPersian($number): array|string
 
     return $number;
 }
-
-
-function priceFormat($price): array|string
-{
-    $price = number_format($price, 0, '/', '،');
-    $price = convertEnglishToPersian($price);
-    return $price;
-}
-
 
 function validateNationalCode($nationalCode): bool
 {
