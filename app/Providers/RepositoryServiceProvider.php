db32025f1909bf35647dce53d48080d495c53760
hamze ghanbari <infogh1000@gmail.com>
2023-10-15 21:27:41 +0330

    add category repository to RepositoryServiceProvider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,28 +2,37 @@
 
 namespace App\Providers;
 
+use App\Repository\Contracts\CategoryRepositoryInterface;
 use App\Repository\Contracts\OtpRepositoryInterface;
 use App\Repository\Contracts\RoleRepositoryInterface;
 use App\Repository\Contracts\UserRepositoryInterface;
+use App\Repository\Eloquent\CategoryRepository;
 use App\Repository\Eloquent\OtpRepository;
 use App\Repository\Eloquent\RoleRepository;
 use App\Repository\Eloquent\UserRepository;
-use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class RepositoryServiceProvider extends ServiceProvider implements DeferrableProvider
+class RepositoryServiceProvider extends ServiceProvider
 {
 
     public array $singletons = [
+        CategoryRepositoryInterface::class => CategoryRepository::class,
         OtpRepositoryInterface::class => OtpRepository::class,
         UserRepositoryInterface::class => UserRepository::class,
-        RoleRepositoryInterface::class => RoleRepository::class
+        RoleRepositoryInterface::class => RoleRepository::class,
     ];
 
     public function register(): void
     {
-        //
+//        $this->app->singleton(CategoryRepositoryInterface::class, function ($app) {
+//            return new CategoryRepository();
+//        });
     }
+
+//    public function provides(): array
+//    {
+//        return [CategoryRepositoryInterface::class];
+//    }
 
     /**
      * Bootstrap services.
