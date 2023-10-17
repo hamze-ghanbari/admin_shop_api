f78d650c815acd5d053a375706301202c63ddce1
hamze ghanbari <infogh1000@gmail.com>
2023-10-17 22:06:18 +0330

    creating brand resource

---
+++ app/Http/Resources/BrandCollection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Traits\ApiPaginationResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class BrandCollection extends ResourceCollection
+{
+    use ApiPaginationResponse;
+
+    public static $wrap = 'result';
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
