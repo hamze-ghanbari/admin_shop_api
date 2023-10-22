66c59efff0565e0edb4485802ece34ef12293c15
hamze ghanbari <infogh1000@gmail.com>
2023-10-22 20:18:01 +0330

    create mails migration

---
+++ database/migrations/2023_10_20_163534_create_mails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->text('body');
+            $table->boolean('status')->default(false);
+            $table->boolean('shipping_status')->default(false);
+            $table->timestamp('published_at');
+            $table->foreignId('user_id')->nullable()->comment('null => public mail userId => send mail for one user')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mails');
+    }
+};
