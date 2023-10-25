b3db82c0c847807682decf197f57932bcdfde102
hamze ghanbari <infogh1000@gmail.com>
2023-10-25 23:18:13 +0330

    update PersianDateCast

--- app/Casts/PersianDateCast.php
+++ app/Casts/PersianDateCast.php
@@ -4,16 +4,17 @@ namespace App\Casts;
 
 use Carbon\Carbon;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Contracts\Database\Eloquent\CastsInboundAttributes;
 use Illuminate\Database\Eloquent\Model;
 use Morilog\Jalali\Jalalian;
 
-class PersianDateCast implements CastsAttributes
+class PersianDateCast implements CastsInboundAttributes
 {
 
-    public function get(Model $model, string $key, mixed $value, array $attributes): string | null
-    {
-        return isset($value) ? jalaliDate($value, 'Y/m/d') : null;
-    }
+//    public function get(Model $model, string $key, mixed $value, array $attributes): string | null
+//    {
+//        return isset($value) ? jalaliDate($value, 'Y/m/d') : null;
+//    }
 
 
     public function set(Model $model, string $key, mixed $value, array $attributes): Carbon
