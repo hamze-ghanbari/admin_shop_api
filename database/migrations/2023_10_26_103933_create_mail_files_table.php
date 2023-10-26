9d000f2fefdbaaf08cb498a55df592d5bb563c73
hamze ghanbari <infogh1000@gmail.com>
2023-10-26 23:41:28 +0330

    create mail_files migration

---
+++ database/migrations/2023_10_26_103933_create_mail_files_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mail_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mail_id')->constrained('mails')->onUpdate('cascade')->onDelete('cascade');
+            $table->text('file_path');
+            $table->bigInteger('file_size');
+            $table->string('file_type');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mail_files');
+    }
+};
