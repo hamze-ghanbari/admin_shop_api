57f0eccabc3cd93a5b010e4323f9783b577b18c2
hamze ghanbari <infogh1000@gmail.com>
2023-10-26 23:40:25 +0330

    update MailService.php

--- app/Http/Controllers/Api/V1/Services/MailService.php
+++ app/Http/Controllers/Api/V1/Services/MailService.php
@@ -6,7 +6,8 @@ use App\Http\Requests\MailRequest;
 use App\Http\Services\CacheApiService\CacheApiService;
 use App\Http\Services\MessageService\Algorithms\Email\EmailService;
 use App\Http\Services\MessageService\MessageService;
-use App\Jobs\SendEmailToUsers;
+use App\Jobs\SendGroupEmail;
+use App\Jobs\SendSingleEmail;
 use App\Mail\PublicMail;
 use App\Models\Mail;
 use App\Repository\Contracts\MailRepositoryInterface;
@@ -53,15 +54,18 @@ class MailService
         return $this->mailRepository->delete($id);
     }
 
-    public function sendMailToUsers(Mail $mail, array $attachments = [])
+    public function dispatchGroupEmail(Mail $mail)
     {
-        SendEmailToUsers::dispatch($mail, $attachments)->onQueue('mailUsers');
+        SendGroupEmail::dispatch($mail)->onQueue('groupMail');
     }
 
-    public function sendMail(Mail $email, array $attachments = [])
+    public function dispatchSingleEmail(Mail $mail){
+        SendSingleEmail::dispatch($mail)->onQueue('singleMail');
+    }
+
+    public function sendGroupMail(Mail $email)
     {
-        // get files from mail files table
-        $files = [];
+
         // get email from newsletters users or users table
         $emails = ['ghanbarih243@gmail.com', 'infogh1000@gmail.com', 'hamzegh5000@gmail.com'];
 
@@ -74,19 +78,43 @@ class MailService
             ->details($details)
             ->subject($email->subject)
             ->mailClass(PublicMail::class)
-            ->files($attachments);
-            if(!isset($email->user_id)) {
-                foreach ($emails as $email) {
-                    $mail->to($email);
-                    $messagesService = new MessageService($emailService);
-                    $messagesService->send();
-                }
-            }else{
-                $mail->to($email->user->email);
+            ->files($this->getAttachments($email->files));
+            foreach ($emails as $email) {
+                $mail->to($email);
                 $messagesService = new MessageService($emailService);
                 $messagesService->send();
             }
+    }
+
+    public function sendSingleMail(Mail $email)
+    {
+        $emailService = new EmailService();
+        $details = [
+            'title' => $email->subject,
+            'body' => $email->body
+        ];
+        $emailService
+            ->details($details)
+            ->subject($email->subject)
+            ->mailClass(PublicMail::class)
+            ->files($this->getAttachments($email->files))
+            ->to($email->user->email);
+
+        $messagesService = new MessageService($emailService);
+        $messagesService->send();
 
     }
 
+    private function getAttachments($mailFiles): array
+    {
+        $files = [];
+        $names = [];
+        $types = [];
+        foreach ($mailFiles as $file){
+            array_push($files, $file->file_path);
+            array_push($names, $file->file_size);
+            array_push($types, $file->file_type);
+        }
+       return setAttachments($files, $names, $types);
+    }
 }
