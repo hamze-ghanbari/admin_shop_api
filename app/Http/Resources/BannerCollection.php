3d09d4ce04fe843736b7edcdd47ff21eef483eb2
hamze ghanbari <infogh1000@gmail.com>
2023-11-07 21:13:10 +0330

    create banner resource

---
+++ app/Http/Resources/BannerCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Traits\Response\ApiPaginationResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class BannerCollection extends ResourceCollection
+{
+    use ApiPaginationResponse;
+    public static $wrap = 'result';
+
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
