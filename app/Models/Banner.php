9ad804580a39df9a306781bc49c7f421e0ad3f59
hamze ghanbari <infogh1000@gmail.com>
2023-11-09 22:51:14 +0330

    update banner model

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Casts\PersianDateCast;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,4 +13,18 @@ class Banner extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
+
+    protected $casts = [
+        'start_date' => PersianDateCast::class,
+        'end_date' => PersianDateCast::class,
+    ];
+
+
+    public function scopeSearch(Builder $query, $term = null)
+    {
+        return $query->when($term, function (Builder $query, $term) {
+            $query->where('title', 'like', "%{$term}%")
+                ->orWhere('url', 'like', "%{$term}%");
+        });
+    }
 }
