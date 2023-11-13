647cfc0780605bb28fcbcc2f6140b7d08a5e85a8
hamze ghanbari <infogh1000@gmail.com>
2023-11-13 21:33:56 +0330

    create delivery model

---
+++ app/Models/Delivery.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder;
+
+class Delivery extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    public function scopeSearch(Builder $query, $term = null)
+    {
+        return $query->when($term, function (Builder $query, $term) {
+            $query->where('name', 'like', "%{$term}%");
+        });
+    }
+}
