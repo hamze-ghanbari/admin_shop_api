6091816cfbc80ba4a908765b6a8f66ae3349ab1f
hamze ghanbari <infogh1000@gmail.com>
2023-11-14 22:33:15 +0330

    create DeliveryRequest

---
+++ app/Http/Requests/DeliveryRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\BlackListRule;
+use App\Traits\Response\ValidationResponse;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class DeliveryRequest extends FormRequest
+{
+    use ValidationResponse;
+
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'max:100', new BlackListRule()],
+            'url' => ['required', new BlackListRule()],
+            'start_date' => ['required', 'date_format:Y/m/d'],
+            'end_date' => ['required', 'date_format:Y/m/d'],
+            'image_path' => 'required',
+            'status' => [Rule::in([1, 2])],
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => 'عنوان',
+            'url' => 'لینک',
+            'start_date' => 'تاریخ شروع نمایش',
+            'end_date' => 'تاریخ پایان نمایش',
+            'image_path' => 'تصویر',
+            'status' => 'وضعیت',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+//            'name.regex' => 'نام برند باید به صورت حروف انگلیسی باشد',
+//            'persian_name.regex' => 'نام برند باید به صورت حروف فارسی باشد',
+//            'image.image' => 'فرمت تصویر نامعتبر است'
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+}
