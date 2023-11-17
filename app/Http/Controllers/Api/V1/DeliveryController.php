a02e8a130efd1e652651f831de179acc67b4b162
hamze ghanbari <infogh1000@gmail.com>
2023-11-17 15:00:42 +0330

    update delivery controller

--- app/Http/Controllers/Api/V1/DeliveryController.php
+++ app/Http/Controllers/Api/V1/DeliveryController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Api\V1\Services\BannerService;
-use App\Http\Requests\BannerRequest;
-use App\Http\Resources\BannerCollection;
-use App\Http\Resources\BannerResource;
+use App\Http\Controllers\Api\V1\Services\DeliveryService;
+use App\Http\Requests\DeliveryRequest;
+use App\Http\Resources\DeliveryCollection;
 use App\Http\Services\ImageService\ImageService;
 use App\Http\Services\PolicyService\PolicyService;
-use App\Models\Banner;
+use App\Models\Delivery;
 use App\Http\Controllers\Controller;
 use App\Traits\Response\ApiResponse;
 use Illuminate\Http\Request;
@@ -19,99 +18,68 @@ class DeliveryController extends Controller
 
     public function __construct(
         public PolicyService $policyService,
-        public BannerService $bannerService
+        public DeliveryService $deliveryService
     )
     {
         $this->middleware('auth:api');
-        $this->middleware('limiter:banner,5')->only('store', 'update', 'changeStatus');
+        $this->middleware('limiter:delivery,5')->only('store', 'update', 'changeStatus');
     }
 
     public function index()
     {
-        if (!$this->policyService->authorize(['admin'], ['read-banner']))
+        if (!$this->policyService->authorize(['admin'], ['read-delivery']))
             return $this->forbiddenResponse();
 
-        return new BannerCollection($this->bannerService->getAllBanners());
+        return new DeliveryCollection($this->deliveryService->getAllDeliverys());
     }
 
-    public function displayableBanners(){
-        if (!$this->policyService->authorize(['admin'], ['read-banner']))
-            return $this->forbiddenResponse();
-
-        return new BannerCollection($this->bannerService->getDisplayableBanners());
-    }
-
-    public function searchBanner(Request $request)
+    public function searchDelivery(Request $request)
     {
-        if (!$this->policyService->authorize(['admin'], ['read-banner']))
+        if (!$this->policyService->authorize(['admin'], ['read-delivery']))
             return $this->forbiddenResponse();
 
-        return new BannerCollection($this->bannerService->searchBanner($request->input('search')));
+        return new DeliveryCollection($this->deliveryService->searchDelivery($request->input('search')));
     }
 
-    public function store(BannerRequest $request)
+    public function store(DeliveryRequest $request)
     {
-        if (!$this->policyService->authorize(['admin'], ['create-banner']))
+        if (!$this->policyService->authorize(['admin'], ['create-delivery']))
             return $this->forbiddenResponse();
 
-        $image = $request->input('image_path');
-        $imageAddress = $this->bannerService->uploadImage($image);
-
-        if (!$imageAddress) {
-            return $this->serverError('خطا در آپلود تصویر');
-        }
-
-        $this->bannerService->createBanner($request, $imageAddress);
+        $this->deliveryService->createDelivery($request);
         return $this->apiResponse(null);
     }
 
-
-    public function show(Banner $banner)
+    public function update(DeliveryRequest $request, Delivery $delivery)
     {
-        if (!$this->policyService->authorize(['admin'], ['read-banner']))
+        if (!$this->policyService->authorize(['admin'], ['update-delivery']))
             return $this->forbiddenResponse();
 
-        return new BannerResource($banner);
-    }
-
-    public function update(BannerRequest $request, Banner $banner)
-    {
-        if (!$this->policyService->authorize(['admin'], ['update-banner']))
-            return $this->forbiddenResponse();
-
-        $image = $request->input('image_path');
-        $imageAddress = $this->bannerService->uploadImage($image);
-
-        if (!$imageAddress) {
-            return $this->serverError('خطا در آپلود تصویر');
-        }
-
-        $this->bannerService->updateBanner($request, $banner->id, $imageAddress);
+        $this->deliveryService->updateDelivery($request, $delivery->id);
         return $this->apiResponse(null);
     }
 
-    public function changeStatus(Banner $banner, $status)
+    public function changeStatus(Delivery $delivery, $status)
     {
-        if (!$this->policyService->authorize(['admin'], ['update-banner']))
+        if (!$this->policyService->authorize(['admin'], ['update-delivery']))
             return $this->forbiddenResponse();
 
-        $updated = $this->bannerService->updateBannerStatus($banner, $status);
+        $updated = $this->deliveryService->updateDeliveryStatus($delivery, $status);
 
         if ($updated) {
             return $this->apiResponse(null);
         } else {
-            return $this->serverError('خطا در ویرایش وضعیت بنر');
+            return $this->serverError('خطا در ویرایش وضعیت روش ارسال');
         }
     }
 
-    public function destroy(ImageService $imageService, Banner $banner)
+    public function destroy(ImageService $imageService, Delivery $delivery)
     {
-        if (!$this->policyService->authorize(['admin'], ['delete-banner']))
+        if (!$this->policyService->authorize(['admin'], ['delete-delivery']))
             return $this->forbiddenResponse();
 
-        $bannerDelete = $this->bannerService->deleteBanner($banner->id);
+        $deliveryDelete = $this->deliveryService->deleteDelivery($delivery->id);
 
-        $this->bannerService->deleteImages($banner->image_path);
-        return $this->apiResponse(null, hasError: (bool)$bannerDelete);
+        return $this->apiResponse(null, hasError: (bool)$deliveryDelete);
     }
 }
