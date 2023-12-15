cb1fe1b2b35bfe4373d2fbf908185fc2b96bd052
hamze ghanbari <infogh1000@gmail.com>
2023-12-15 21:57:40 +0330

    update GalleryProduct model

--- app/Models/GalleryProduct.php
+++ app/Models/GalleryProduct.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\PersianDateCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,6 +12,10 @@ class GalleryProduct extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
+
+    protected $casts = [
+        'image' => 'array',
+    ];
 
     public function product(){
         return $this->belongsToMany(Product::class);
