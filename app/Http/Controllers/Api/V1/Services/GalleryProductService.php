6b62e67344a6672dacb3d8703ccde77810b1a7cb
hamze ghanbari <infogh1000@gmail.com>
2023-12-15 21:57:57 +0330

    update GalleryProduct service

--- app/Http/Controllers/Api/V1/Services/GalleryProductService.php
+++ app/Http/Controllers/Api/V1/Services/GalleryProductService.php
@@ -6,31 +6,57 @@ use App\Http\Requests\ColorProductRequest;
 use App\Http\Requests\GalleryProductRequest;
 use App\Http\Requests\MetaProductRequest;
 use App\Http\Services\CacheApiService\CacheApiService;
+use App\Http\Services\ImageService\ImageService;
+use App\Http\Services\UploadService\Algoritms\Base64File;
+use App\Http\Services\UploadService\UploadService;
 use App\Models\Product;
 use App\Repository\Contracts\ColorProductRepositoryInterface;
 use App\Repository\Contracts\GalleryProductRepositoryInterface;
 use App\Repository\Contracts\MetaProductRepositoryInterface;
+use Illuminate\Support\Facades\File;
 
 class GalleryProductService
 {
 
     public function __construct(
         public GalleryProductRepositoryInterface $galleryProductRepository,
-        public CacheApiService                $cacheApiService
+        public CacheApiService                $cacheApiService,
+        public ImageService $imageService
     )
     {
     }
 
-    public function addImageToProduct(GalleryProductRequest $request, $productId)
+    public function addImageToProduct($imageAddress, $productId)
     {
-        $this->galleryProductRepository->create($request->fields(attributes: [
+        $this->galleryProductRepository->create([
+            'image' => $imageAddress,
             'product_id' => $productId
-        ]));
+        ]);
     }
 
-    public function deleteImageProduct($colorId)
+    public function deleteImageProduct($imageId)
     {
-        return $this->galleryProductRepository->delete($colorId);
+        return $this->galleryProductRepository->delete($imageId);
+    }
+
+    public function deleteImage($imagePath)
+    {
+        if(isset($imagePath)) {
+            foreach ($imagePath['indexArray'] as $path) {
+                $this->imageService->deleteIndex($path);
+            }
+        }
+    }
+
+    public function uploadFile($image)
+    {
+        $this->imageService->setExclusiveDirectory('uploads' . DIRECTORY_SEPARATOR . 'products');
+
+        $file = new Base64File($this->imageService, $image);
+        $upload = new UploadService($file);
+        $imageAddress = $upload->uploadIndexFile();
+
+        return $imageAddress ?? false;
     }
 
 }
